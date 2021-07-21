31362a5558941d6cdbc34a9118f855756c365144
matias.nunez <matias.nunez@intraway.com>
2021-07-21 09:56:19 -0300

    first commit

---
+++ src/main/java/com/example/DemoGraphQL/resolver/BookResolver.java
@@ -0,0 +1,20 @@
+package com.example.DemoGraphQL.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.example.DemoGraphQL.model.Author;
+import com.example.DemoGraphQL.model.Book;
+import com.example.DemoGraphQL.repository.AuthorRepository;
+
+import java.util.Optional;
+
+public class BookResolver implements GraphQLResolver<Book> {
+    private AuthorRepository authorRepository;
+
+    public BookResolver(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public Author getAuthor(Book book) {
+        return authorRepository.findById(book.getAuthor().getId()).orElse(null);
+    }
+}
